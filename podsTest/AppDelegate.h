dc679994f56265392c6cf3f2682f6c3ed934455d
马浩哲 <mahaozhe@mahaozhedemacbook-pro.local>
2016-05-24 11:44:01 +0800

    Initial Commit

---
+++ podsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podsTest
+//
+//  Created by 马浩哲 on 16/5/24.
+//  Copyright © 2016年 MHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podsTest
+//
+//  Created by 马浩哲 on 16/5/24.
+//  Copyright © 2016年 MHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
